a75ec5ee9bf3f53102d86aae49cbe1fa37497004
Freelancerforcode <125452651+Freelancerforcode@users.noreply.github.com>
2025-04-20 07:18:04 +0100

    Update video-info.js

---
+++ api/video-info.js
@@ -0,0 +1,8 @@
+{
+  "title": "Actual Video Title",
+  "thumbnail": "https://.../thumbnail.jpg",
+  "duration": "10:30",
+  "formats": [
+    { "quality": "1080p", "type": "MP4", "size": "45MB", "url": "https://.../download.mp4" }
+  ]
+}
